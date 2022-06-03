faaaa8d4fd63b7e6da156c647decf55a869fcfb7
helina belete <helinabmeaza@gmail.com>
2022-06-03 16:10:17 +0300

    4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+	if (letter != 'q' || letter != '')
+		putchar(leter);
+
+putchar('\n');
+
+return (0);
+}
