9c190eabb07891d7501da8d249a9888715b4f4dd
helina belete <helinabmeaza@gmail.com>
2022-06-07 14:13:53 +0300

    0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,11 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[] = "_putchar";
+	for ( int c = 0; c < 8; c++){
+_putchar(c[c]);
+	}
+
 	_putchar('\n');
 
 	return (0);
