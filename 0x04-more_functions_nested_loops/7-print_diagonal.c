2d52dacfd6a089260895ff058451600b35b758d7
helina belete <helinabmeaza@gmail.com>
2022-06-09 12:23:35 +0300

    Adding 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,30 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_diagonal - Draws a diagonal line using the \ character.
- * @n: The number of \ characters to be printed.
- */
+  * print_diagonal - Draws a diagonal lines according parameter
+  * @n: The number of times to print diagonal lines
+  *
+  * Return: empty
+  */
 void print_diagonal(int n)
 {
-	int len, space;
+	int x, y;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (len = 0; len < n; len++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
 		{
-			for (space = 0; space < len; space++)
-				_putchar(' ');
-			_putchar('\\');
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
 
-			if (len == n - 1)
-				continue;
-
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
-
-	_putchar('\n');
 }
