8fb7bf98becea1f371013a295e44aa5cb524abd6
helina belete <helinabmeaza@gmail.com>
2022-06-13 21:57:42 +0300

    Adding 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,17 +5,16 @@
  */
 void puts_half(char *str)
 {
-	int i, j;
+	int j, i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i))
 		i++;
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = (i + 1) / 2;
+	j = i / 2;
+	if (i % 2)
+		j += 1;
 	while (j < i)
 	{
-		_putchar(str[j]);
+		_putchar(*(str + j));
 		j++;
 	}
 	_putchar('\n');
