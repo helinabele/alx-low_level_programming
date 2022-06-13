4cfb24f2457fc386b23c74c8d982b5db991d2fc3
helina belete <helinabmeaza@gmail.com>
2022-06-13 13:41:56 +0300

    Adding 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	 int i = 0;
+
+	 while(s != '\0'){
+	 i++;
+	 s++;
+	 }
+	 return (i);
+}
