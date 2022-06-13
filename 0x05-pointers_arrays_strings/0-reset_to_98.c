a80171a612f4d17d1fef2eae0293bea6fc455317
helina belete <helinabmeaza@gmail.com>
2022-06-13 11:14:27 +0300

    0x05-pointers_arrays_strings first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main,h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates value of pointer to 98.
+ * @n: input to update to 98.
+ */
+
+void reset_to_98(int n)
+{
+	*n = 98;	
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05-pointers_arrays_strings project June 13, 2022
