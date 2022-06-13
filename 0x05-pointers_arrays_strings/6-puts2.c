a9c7ea4338ce12c11410a4afce7e16809f7137ea
helina belete <helinabmeaza@gmail.com>
2022-06-13 21:49:18 +0300

    Adding 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,12 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i))
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		_putchar(*(str+i));
+		i = i + 2;
 	}
 	_putchar('\n');
 }
