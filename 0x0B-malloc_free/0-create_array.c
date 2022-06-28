b86d5466790873d5535683151123017171c251ab
helina belete <helinabmeaza@gmail.com>
2022-06-28 19:31:09 +0300

    Malloc Free function June 28 2022

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size > 0)
+	{
+		p = malloc(size * sizeof(char));
+		if (p == NULL)
+			return (NULL);
+	}
+	else
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
+}
