5973d1e04815dd7840dd6568bf7075a958d75194
helina belete <helinabmeaza@gmail.com>
2022-06-28 19:45:42 +0300

    File revamped

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,21 +8,22 @@
  *
  * Return: Nothing.
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *p;
 	unsigned int i;
+	char *a;
 
-	if (size > 0)
-	{
-		p = malloc(size * sizeof(char));
-		if (p == NULL)
-			return (NULL);
-	}
-	else
+	if (size == 0)
 		return (NULL);
+	a = malloc(size * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
-		p[i] = c;
-	return (p);
+	{
+		a[i] = c;
+	}
+	return (a);
 }
+© 2022 Git
