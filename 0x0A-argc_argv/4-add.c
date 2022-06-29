2243a1fe97c00f6ddff9ac8942f928a1edea7747
helinabele <helinabmeaza@gmail.com>
2022-06-29 11:26:41 +0300

    Argc ArgV

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,33 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
- * main - adds positive numbers
- * @argc: argument count
- * @argv: argument vector for values
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * Return: Always 0 (Success)
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
+	int a = 0, i, j;
 
-	sum = 0;
-
-	for (i = 1; i < argc ; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0' ; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (argv[i][j] < 47 || argv[i][j] > 57)
+			if (isdigit(argv[i][j]) == 0)
 			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
 		}
-		sum = sum + atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
 	return (0);
 }
