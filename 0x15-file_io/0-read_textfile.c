c71a6fd5ae225a46f3e614273e20ca770606daa8
helina belete <helinabmeaza@gmail.com>
2022-07-25 20:47:18 +0300

    File 0-read_textfile.c added

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,17 @@
-#include <main.h>
-#include <stdlib.h>
-//open requires 3 header files
+#include "main.h"
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnt1.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-#include <unistd.h>
-
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
